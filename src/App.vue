f9623abb97ea82d0da12936f0d9db2e245c9ab6e
Thomas <chearful@chearfulcraft.org>
2021-04-28 20:46:23 +0100

    First release

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,100 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container text-center">
+        <h1>Which runway(s) will Heathrow be using?</h1>
+        <h6>Heathrow should currently be taking off from the {{ current.departure.runway }}ern runway heading towards {{ current.departure.direction }} and landing on the {{ current.arrival.runway }}ern runway over {{ current.arrival.direction }}</h6>
+        <hr>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div v-for="day in ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']" style="width:14.2857%"><h3>{{ day }}</h3></div>
+            <div :style="'width: ' + (offset_days*14.2857) + '%'"></div>
+            <div v-for="forecast in forecasts" style="width:14.2857%">
+                <h5 :style="(forecast.easterly ? 'color:green' : '')">{{ forecast.date }}</h5>
+                <p>Departures: {{ forecast.runways.departures[0] }}/{{ forecast.runways.departures[1] }}</p>
+                <p>Arrivals: {{ forecast.runways.arrivals[0] }}/{{ forecast.runways.arrivals[1] }}</p>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <hr>
+        <div class="row">
+            <div class="col-lg-4">
+                <h3>Runway Alternation</h3>
+                <p>At 3pm each day, the runway used for departures and landings alternates. This means that the runway used before 3pm for arrivals will become the runway used for departures. This alternates each week, so one week departures may be on the southern runway before 3pm with the next week having departures on the southern runway after 3pm. Runway alternation does not take place on easterly operations.</p>
+            </div>
+            <div class="col-lg-4">
+                <h3>Wind Direction</h3>
+                <p>Aircraft must take off and land into the wind. If the wind is coming from the east, Heathrow switches to what is known as easterly operations and aircraft take off to the east (i.e. over London). Alternation does not take place on easterly operations as the taxiway infrastructure is insufficient. The infrastructure was not developed as a result of the Cranford Agreement, which whilst it has since expired, prevented aircraft from taking off over Cranford (i.e. 09L departures). Heathrow operates on a 'westerly preference'. This means that even if the wind is coming from the east, if the winds are light (less than 5kts, generally), Heathrow will continue to operate on westerly operations.</p>
+            </div>
+            <div class="col-lg-4">
+                <h3>Single Runway Operations</h3>
+                <p>As a result of drastically reduced flight schedules due to COVID-19, Heathrow is only using one runway for both arrivals and departures. The runway used alternates on a weekly basis. The westerly preference does not operate as part of single runway operations.</p>
+                <h3>More Info</h3>
+                Live updates: <a href="https://twitter.com/HeathrowNoise">@HeathrowNoise</a><br>
+                More information: <a href="https://www.heathrow.com/company/local-community/noise/operations">heathrow.com</a>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data: function() {
+      return {
+        forecasts: [],
+        current: {departure: {}, arrival: {}},
+        offset_days: 0
+      }
+  },
+  mounted() {
+        Date.prototype.getWeekNumber = function(){
+            let d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
+            let dayNum = d.getUTCDay() || 7;
+            d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+            let yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+            return Math.ceil((((d - yearStart) / 86400000) + 1)/7)
+        };
+
+    fetch("https://api.weatherbit.io/v2.0/forecast/daily?&city=London&country=UK&key=2e562a75b5934c92b46a0146ba921871").then(response => response.json())
+    .then(data => {
+        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+        let offsetMap = [6, 0, 1, 2, 3, 4, 5];
+        this.offset_days = offsetMap[new Date(data.data[0].datetime).getDay()]
+
+        let singleRunwayOperations = true;
+
+        for(let forecast of data.data) {
+            let date = new Date(forecast.datetime);
+            let isEvenWeek = (date.getWeekNumber() % 2 == 0);
+            let easterly = forecast.wind_dir > 22.5 && forecast.wind_dir < 157.5 && forecast.wind_gust_spd >= 3;
+            let runways = {};
+            if(singleRunwayOperations) {
+                if(isEvenWeek && !easterly) runways = {departures: ["27R", "27R"], arrivals: ["27R", "27R"]};
+                if(isEvenWeek && easterly) runways = {departures: ["09L", "09L"], arrivals: ["09L", "09L"]};
+                if(!isEvenWeek && !easterly) runways = {departures: ["27L", "27L"], arrivals: ["27L", "27L"]};
+                if(!isEvenWeek && easterly) runways = {departures: ["09R", "09R"], arrivals: ["09R", "09R"]};
+            }
+
+            if(this.current.departure.direction == null) {
+                let isAfter3 = new Date().toLocaleString("en-GB", {hour: "2-digit", hour12: false, timeZone: "Europe/London"}) >= 15 ? 1 : 0;
+
+                this.current.departure.direction = easterly ? "London" : "Windsor";
+                this.current.arrival.direction = easterly ? "Windsor" : "London";
+
+                this.current.arrival.runway = (runways.arrivals[isAfter3] == "09L" || runways.arrivals[isAfter3] == "27R") ? "north" : "south";
+                this.current.departure.runway = (runways.departures[isAfter3] == "09L" || runways.departures[isAfter3] == "27R") ? "north" : "south";
+            }
+
+            this.forecasts.push({date: date.getDate() + "/" + months[date.getMonth()], easterly: easterly, runways: runways})
+        }
+    });
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
