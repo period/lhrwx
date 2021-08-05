c32bcaa0b882ba90f56800ea52888ca3cf3855d9
Thomas <chearful@chearfulcraft.org>
2021-08-05 16:01:29 +0100

    Various formatting fixes

--- src/App.vue
+++ src/App.vue
@@ -40,18 +40,18 @@
   </div>
 </template>
 <script>
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css"
 export default {
-  name: 'App',
-  components: {
-  },
-  data: function() {
-      return {
-        weeks: [{forecasts:[],offset_days:0,offset_days_post:0}],
-        current: {departure: {}, arrival: {}}
-     }
-  },
-  mounted() {
+    name: "App",
+    components: {
+    },
+    data: function() {
+        return {
+            weeks: [{forecasts:[],offset_days:0,offset_days_post:0}],
+            current: {departure: {}, arrival: {}}
+        }
+    },
+    mounted() {
         Date.prototype.getWeekNumber = function(){
             let d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
             let dayNum = d.getUTCDay() || 7;
@@ -60,47 +60,49 @@ export default {
             return Math.ceil((((d - yearStart) / 86400000) + 1)/7)
         };
 
-    fetch("https://api.weatherbit.io/v2.0/forecast/daily?&city=London&country=UK&key=2e562a75b5934c92b46a0146ba921871").then(response => response.json())
-    .then(data => {
-        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        fetch("https://api.weatherbit.io/v2.0/forecast/daily?&city=London&country=UK&key=2e562a75b5934c92b46a0146ba921871").then(response => response.json()).then(data => {
+            let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
-        let offsetMap = [6, 0, 1, 2, 3, 4, 5];
-        this.weeks[0].offset_days = offsetMap[new Date(data.data[0].datetime).getDay()]
+            let offsetMap = [6, 0, 1, 2, 3, 4, 5];
+            this.weeks[0].offset_days = offsetMap[new Date(data.data[0].datetime).getDay()]
 
-        let singleRunwayOperations = true;
+            let singleRunwayOperations = true;
 
-        let weekOffset = new Date().getWeekNumber();
-        
+            let weekOffset = new Date().getWeekNumber();
+            
 
-        for(let forecast of data.data) {
-            let date = new Date(forecast.datetime);
-            let isEvenWeek = (date.getWeekNumber() % 2 == 0);
-            let easterly = forecast.wind_dir > 22.5 && forecast.wind_dir < 157.5 && forecast.wind_gust_spd >= 3;
-            let runways = {};
-            if(singleRunwayOperations) {
-                if(isEvenWeek && !easterly) runways = {departures: ["27R", "27L"], arrivals: ["27L", "27R"]};
-                if(isEvenWeek && easterly) runways = {departures: ["09R", "09R"], arrivals: ["09L", "09L"]};
-                if(!isEvenWeek && !easterly) runways = {departures: ["27L", "27R"], arrivals: ["27R", "27L"]};
-                if(!isEvenWeek && easterly) runways = {departures: ["09R", "09R"], arrivals: ["09L", "09L"]};
+            for(let forecast of data.data) {
+                let date = new Date(forecast.datetime);
+                let isEvenWeek = (date.getWeekNumber() % 2 == 0);
+                let easterly = forecast.wind_dir > 22.5 && forecast.wind_dir < 157.5 && forecast.wind_gust_spd >= 3;
+                let runways = {};
+                if(singleRunwayOperations) {
+                    if(isEvenWeek && !easterly) runways = {departures: ["27R", "27L"], arrivals: ["27L", "27R"]};
+                    if(isEvenWeek && easterly) runways = {departures: ["09R", "09R"], arrivals: ["09L", "09L"]};
+                    if(!isEvenWeek && !easterly) runways = {departures: ["27L", "27R"], arrivals: ["27R", "27L"]};
+                    if(!isEvenWeek && easterly) runways = {departures: ["09R", "09R"], arrivals: ["09L", "09L"]};
+                }
+
+                if(this.current.departure.direction == null) {
+                    let isAfter3 = new Date().toLocaleString("en-GB", {hour: "2-digit", hour12: false, timeZone: "Europe/London"}) >= 15 ? 1 : 0;
+
+                    this.current.departure.direction = easterly ? "London" : "Windsor";
+                    this.current.arrival.direction = easterly ? "Windsor" : "London";
+
+                    this.current.arrival.runway = (runways.arrivals[isAfter3] == "09L" || runways.arrivals[isAfter3] == "27R") ? "north" : "south";
+                    this.current.departure.runway = (runways.departures[isAfter3] == "09L" || runways.departures[isAfter3] == "27R") ? "north" : "south";
+                }
+                console.log("offset: " + (date.getWeekNumber()-weekOffset));
+                if(this.weeks[date.getWeekNumber()-weekOffset] == null) {
+                    this.weeks[date.getWeekNumber()-weekOffset] = {forecasts:[], offset_days: 0, offset_days_post: 0}
+                }
+                this.weeks[date.getWeekNumber()-weekOffset].forecasts.push({date: date.getDate() + "/" + months[date.getMonth()], easterly: easterly, runways: runways})
             }
+            this.weeks[this.weeks.length-1].offset_days_post = 7 - this.weeks[this.weeks.length-1].forecasts.length
+        });
+        fetch("https://lhrwx.thomas.gg/forecast.json").then(response => response.json()).then(data => {
 
-            if(this.current.departure.direction == null) {
-                let isAfter3 = new Date().toLocaleString("en-GB", {hour: "2-digit", hour12: false, timeZone: "Europe/London"}) >= 15 ? 1 : 0;
-
-                this.current.departure.direction = easterly ? "London" : "Windsor";
-                this.current.arrival.direction = easterly ? "Windsor" : "London";
-
-                this.current.arrival.runway = (runways.arrivals[isAfter3] == "09L" || runways.arrivals[isAfter3] == "27R") ? "north" : "south";
-                this.current.departure.runway = (runways.departures[isAfter3] == "09L" || runways.departures[isAfter3] == "27R") ? "north" : "south";
-            }
-            console.log("offset: " + (date.getWeekNumber()-weekOffset));
-            if(this.weeks[date.getWeekNumber()-weekOffset] == null) {
-                this.weeks[date.getWeekNumber()-weekOffset] = {forecasts:[], offset_days: 0, offset_days_post: 0}
-            }
-            this.weeks[date.getWeekNumber()-weekOffset].forecasts.push({date: date.getDate() + "/" + months[date.getMonth()], easterly: easterly, runways: runways})
-        }
-        this.weeks[this.weeks.length-1].offset_days_post = 7 - this.weeks[this.weeks.length-1].forecasts.length
-    });
+        });
   }
 }
 </script>
